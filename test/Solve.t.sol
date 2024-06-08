e2c9600ef6c4c1b9e35a4fc9805bc43c0892bd46
Moustafa El-Tohamy <65693055+mooezz999@users.noreply.github.com>
2024-06-08 18:10:47 +0300

    Update Solve.t.sol

--- test/Solve.t.sol
+++ test/Solve.t.sol
@@ -8,7 +8,7 @@ contract SolveTest is Test {
     Solve public solve;
 
     function setUp() public {
-        solve = new Solve(0xf988Ebf9D801F4D3595592490D7fF029E438deCa);
+        solve = new Solve();
         
     }
 
@@ -18,6 +18,7 @@ contract SolveTest is Test {
     }
 
     function test_canTransferNft() public {
+        solve.soolve();
         solve.safeTransfer();
     }
 }
